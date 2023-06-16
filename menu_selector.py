edd5b8f859dbc671af6f0e741d4e7c822218a8c5
GoAheadMan <hataway072@gmail.com>
2023-06-16 13:17:52 +0900

    version#2

--- menu_selector.py
+++ menu_selector.py
@@ -1,4 +1,9 @@
+import file_manager
+import parking_spot_manager
+
 def start_process(path):
+    str_list = file_manager.read_file(path) #path 경로로 파일을 읽어와 리스트 생성
+    spots = parking_spot_manager.str_list_to_class_list(str_list)
     while True:
         print("---menu---")
         print("[1] print")
@@ -6,9 +11,8 @@ def start_process(path):
         print("[3] sort")
         print("[4] exit")
         select = int(input('type:'))
-        if select == 1:
-            print("not implemented yet")
-            # fill this block
+        if select == 1: # 전체 목록 출력
+            parking_spot_manager.print_spots(spots)
         elif select == 2:
             print("---filter by---")
             print("[1] name")
@@ -51,8 +55,8 @@ def start_process(path):
                 print("not implemented yet")
                 # fill this block
             else: print("invalid input")
-        elif select == 4:
-            print("not implemented yet")
-            # fill this block
+        elif select == 4: # Exit를 출력하고 종료
+            print("Exit") 
+            break
         else:
             print("invalid input")
--- parking_spot_manager.py
+++ parking_spot_manager.py
@@ -6,7 +6,26 @@ class parking_spot:
         s += f"{item['city']} {item['district']}"
         s += f"(lat:{item['latitude']}, long:{item['longitude']})"
         return s
+    def __init__(self, name, city, district, ptype, longitude, latitude): # __item 필드 생성을 위한 생성자
+        self.__item = {'name': name, 'city': city, 'district': district, 'ptype': ptype, 'longitude': longitude, 'latitude': latitude}
+        
+    def get(self, keword = 'name') : # get 메소드, 기본인수는 'name'
+        return self.__item[keword]
 
+
+def str_list_to_class_list (str_list):
+    class_list = []; # parking_spot 클래스를 담을 새로운 리스트
+    
+    for item in str_list:
+        arr = item.split(',') # ',' 단위로 쪼개서 저장
+        class_list.append(parking_spot(arr[1], arr[2], arr[3], arr[4], arr[5], arr[6])) # arr[0]은 순번이라 arr[1]부터 저장
+
+    return class_list
+    
+def print_spots(spots) :
+    print("---print elements({})---".format(len(spots))) # 총 길이 출력
+    for parking_spots in spots :
+        print(parking_spots)
 
 # 각 단계별로 테스트 (테스트할때 주석해제 후 사용)
 if __name__ == '__main__':
